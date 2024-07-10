3c1ac3fab9379249e4318933bce04c402345d9a5
Victoria Okonnah <victoriaokonnah@gmail.com>
2024-07-10 16:51:51 +0100

    test

--- src/server.ts
+++ src/server.ts
@@ -58,3 +58,19 @@ app.listen(process.env.PORT || 5000, async () => {
 //             "use": "@vercel/node"
 //         }
 //     ],
+
+// {
+//     "version": 2,
+//     "builds": [
+//         {
+//             "src": "/dist/server.js",
+//             "use": "@vercel/node"
+//         }
+//     ],
+//     "routes": [
+//         {
+//              "src": "/(.*)",
+//               "dest": "/dist/server.js"
+//         }
+//     ]
+// }
