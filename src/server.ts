08e2233af8bc165f4ec2b16b8f4b049ea49143bf
Victoria Okonnah <victoriaokonnah@gmail.com>
2024-07-11 13:05:11 +0100

    test

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import * as dotenv from "dotenv"
 dotenv.config();
-import express, { Request, Response } from "express"
+import express, { NextFunction, Request, Response } from "express"
 import sequelize from "./database/connection";
 import userRouter from "./routes/userRoute";
 import OrganizationRoute from "./routes/organizationRoute";
@@ -16,6 +16,19 @@ app.use("/auth",userRouter);
 app.use("/api",OrganizationRoute);
 app.use("*",(req:Request,res:Response) => {
     return res.status(404).json({sucess: false, message: "Route not found"});
+})
+app.use((err:any,req:Request,res:Response,next:NextFunction) => {
+    console.log({msg:err.message});
+    let status =  res.statusCode || 500;
+    if(err.message == 'fetch failed'){
+        status = 400;
+    }
+    let message = err.message || "Internal Server Error";
+    if(err.code == 11000){
+        status = 400
+        message = "You have reviewed this book already"
+    }
+    return res.status(status).json({message})
 })
 
 app.listen(process.env.PORT || 5000, async () => {
