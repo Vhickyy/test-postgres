6d92d36f179f8f10e1d62142f146f5cdd4f00c17
Victoria Okonnah <victoriaokonnah@gmail.com>
2024-07-11 12:09:08 +0100

    test

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,11 +1,23 @@
-import pg from 'pg';
 import { Sequelize } from "sequelize";
+import * as pg from "pg"; 
 import * as dotenv from "dotenv";
 
 dotenv.config()
-
-const sequelize = new Sequelize(process.env.URI!, {
-    dialect: 'postgres'
+// process.env.URI!
+const sequelize = new Sequelize(
+    process.env.URI!,
+    process.env.USERNAME!,
+    process.env.POSTGRESDB!
+    , {
+    dialect: 'postgres',
+    dialectModule: pg,
+    port: Number(process.env.DB_PORT!),
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    }
 })
 
 export default sequelize;
